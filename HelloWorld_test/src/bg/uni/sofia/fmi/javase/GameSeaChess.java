100a76aeb7f63feae663bfa5f6efc964ed9d41db
Nikola <Nikola@NIKOLA>
2016-10-22 15:08:11 +0300

    Fixed a lot of errors

--- HelloWorld_test/src/bg/uni/sofia/fmi/javase/GameSeaChess.java
+++ HelloWorld_test/src/bg/uni/sofia/fmi/javase/GameSeaChess.java
@@ -1,8 +1,10 @@
 package bg.uni.sofia.fmi.javase;
-import bg.uni.sofia.fmi.javase.game;
-public class HelloWorld {
+import bg.uni.sofia.fmi.javase.SeaChess;
+public class GameSeaChess
+{
 	public static void main(String[] args) {
-		game test = new game();
+		SeaChess test = new SeaChess();
 		test.play();
 	}
+	
 }
